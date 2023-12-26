96bac5d5a5dfa00c7b4c76c32bf4b489d8c52b40
RCLobster <44736025+RCLobster@users.noreply.github.com>
2023-12-26 10:44:37 -0800

    once any save btn is clicked, grab a reference to the textarea sibling element of the clicked btn

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* THE PLAN
 --1. curent day and time displayed at top of page (working clock)
-2. each time block is color coded to indicate past, present, or future 
-(changes dynamically with time)
+--2. each time block is color coded to indicate past, present, or future 
+--(changes dynamically with time)
 --3. clicking in a time block lets user type
 4. when save button is clicked, the text in THAT block is saved to local storage
 5. on page reload, saved events persist
@@ -17,6 +17,9 @@ var hour14_ID = $("#hour-14");
 var hour15_ID = $("#hour-15");
 var hour16_ID = $("#hour-16");
 var hour17_ID = $("#hour-17");
+
+var description = $(".description");
+var save_BTN = $(".saveBtn");
 
 //VARIABLES
 var militaryClock = dayjs().format("H");
@@ -34,7 +37,7 @@ var plannerHours = [
   hour16_ID,
   hour17_ID 
 ];
-//array of numbers corresponding to the time present in each div with an hourI_D
+//array of numbers corresponding to the time present in each div with an hour_ID
 var numbersArray = [9,10,11,12,13,14,15,16,17];
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
@@ -47,15 +50,22 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  //  
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
   
-  console.log("current military time: " + Number(militaryClock));
+  save_BTN.on("click", function(){
+    //grab a reference to the textarea attached to the saveBTN that was clicked
+    var contentToSave = $(this).prev();
+    console.log(contentToSave.val());
 
+  })
+  
+  //dynamically update BG color depending on if time block is past, present, or future
+  //console.log("current military time: " + Number(militaryClock));
   for(var x = 0; x < numbersArray.length; x++){
     /*if military time is larger than the number at numbersArray[x]
           set the class of those elements in the correseponding plannerHours[] to "future"
@@ -72,13 +82,6 @@ $(function () {
       plannerHours[x].parent().parent().attr("class", "row time-block present");
     }
   }
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
   //working timer which displays current day, date, and time at top of page
   function displayDateTime() {
     timer = setInterval(function(){
